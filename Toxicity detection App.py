071052a0485644aa79d330abc985c37381f5d9da
Sridevivaradharajan <159702077+Sridevivaradharajan@users.noreply.github.com>
2025-08-26 12:00:50 +0530

    Update Toxicity detection App.py

--- Toxicity detection App.py
+++ Toxicity detection App.py
@@ -400,22 +400,34 @@ if 'current_page' not in st.session_state:
 
 @st.cache_resource
 def load_bilstm_model():
-    try:
-        model = tf.keras.models.load_model("bilstm_model.h5")
-        return model
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
-        return None
+    model_path = "bilstm_model.h5"
 
+    if not os.path.exists(model_path):
+        with st.spinner("Downloading BiLSTM model... Please wait."):
+            # Use direct download link (replace with your file_id)
+            file_id = "1qJnieGfXN3zz6vpqcYV7uNDVp5FFHCVf"
+            url = f"https://drive.google.com/uc?id={file_id}"
+            gdown.download(url, model_path, quiet=False)
+
+    # return loaded model
+    return load_model(model_path)
+
+# Load Tokenizer
 @st.cache_resource
 def load_custom_tokenizer():
-    try:
-        # Use the static load method from your CustomTokenizer class
-        tokenizer = CustomTokenizer.load("custom_tokenizer.pkl")
-        return tokenizer
-    except Exception as e:
-        st.error(f"Error loading custom tokenizer: {e}")
-        return None
+    tokenizer_path = "custom_tokenizer.pkl"
+
+    if not os.path.exists(tokenizer_path):
+        with st.spinner("Downloading tokenizer... Please wait."):
+            file_id = "19Bwmqij3CLSYsGOT1Z7kVMovsuM8CHlY"
+            url = f"https://drive.google.com/uc?id={file_id}"
+            gdown.download(url, tokenizer_path, quiet=False)
+
+    # Debugging check
+    with open(tokenizer_path, "rb") as handle:
+        tokenizer = pickle.load(handle)
+
+    return tokenizer
 
 # Load model and tokenizer
 model = load_bilstm_model()
@@ -1279,6 +1291,7 @@ elif current_page == 'Test Cases':
                         st.error(f"**TOXIC** - {toxic_count} categories detected!")
                     else:
                         st.success("**CLEAN** - No toxicity detected!")
+
 
 
 
